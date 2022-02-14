b4d004a227d6fbc6225c48eb4fc9cb1ec3942b6a
Francis <fmoscatelli@hotmail.com>
2022-02-14 11:55:05 -0300

    added next/prev buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,48 +1,61 @@
-import React, { useState } from 'react';
+// eslint-disable-next-line no-unused-vars
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { MdNavigateBefore, MdNavigateNext } from 'react-icons/md';
 import Card from './Card';
 
 const ContenedorNavBar = styled.div`
-display: flex;
-justify-content: space-between;
-margin: 0 5px 5px 5px;
-bottom: 0; 
+  display: flex;
+  justify-content: space-between;
+  margin: 0 5px 5px 5px;
+  bottom: 0;
 `;
 const ButtonNav = styled.button`
-border-radius: 20px;
-border-width: 3px;
-box-shadow: 1px 2px 5px rgb(0, 0, 0, 0.3);
-background-color: #2a75bb;
-border-color: #ffcb05;
-height: 50px;
-width: 50px;
+  border-radius: 20px;
+  border-width: 3px;
+  box-shadow: 1px 2px 5px rgb(0, 0, 0, 0.3);
+  background-color: #2a75bb;
+  border-color: #ffcb05;
+  height: 50px;
+  width: 50px;
 `;
 
 const Back = styled(MdNavigateBefore)`
-height: 40px;
-width: auto;
-color: #ffcb05;
+  height: 40px;
+  width: auto;
+  color: #ffcb05;
 `;
 
 const Next = styled(MdNavigateNext)`
-height: 40px;
-width: auto;
-color: #ffcb05;
+  height: 40px;
+  width: auto;
+  color: #ffcb05;
 `;
 
 const Home = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [elementos, setElementos] = useState([...Array(20).keys()]);
+  const [elementos, setElementos] = useState(
+    Array.from({ length: 20 }, (_, i) => i + 1),
+  );
+
   return (
     <div>
-      {
-        elementos.map((elemento) => <Card key={elemento} numero={elemento + 1} />)
-      }
+      {elementos.map((elemento) => (
+        <Card key={elemento} numero={elemento} />
+      ))}
       <ContenedorNavBar>
-        {/* { onClick={setElementos(useEffect(() => elementos.map((el) => el + 20), []))}} */}
-        <ButtonNav type="button"><Back /></ButtonNav>
-        <ButtonNav type="button"><Next /></ButtonNav>
+        <ButtonNav
+          type="button"
+          onClick={() => setElementos(Array.from(elementos, (el) => el - 20))}
+        >
+          <Back />
+        </ButtonNav>
+        <ButtonNav
+          type="button"
+          // ultimo disponible 898
+          onClick={() => setElementos(Array.from(elementos, (el) => el + 20))}
+        >
+          <Next />
+        </ButtonNav>
       </ContenedorNavBar>
     </div>
   );
