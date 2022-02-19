0e582cb0c99bef66fe2b11d555e705cfa3e4d912
Francis <fmoscatelli@hotmail.com>
2022-02-19 14:38:42 -0300

    Delete counter reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import favoriteReducer from '../features/favorite/favoriteSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     favorite: favoriteReducer,
   },
 });
