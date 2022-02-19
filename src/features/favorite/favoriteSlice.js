b1195a3420738bf4d800305797bc6ee92535a717
Francis <fmoscatelli@hotmail.com>
2022-02-19 18:31:33 -0300

    fix elements repeat

--- src/features/favorite/favoriteSlice.js
+++ src/features/favorite/favoriteSlice.js
@@ -4,12 +4,23 @@ export const favoriteSlice = createSlice({
   name: 'favorite',
   initialState: [],
   reducers: {
-    addFavorite: (state, action) => {
+    addElement: (state, action) => {
       state.push(action.payload);
     },
   },
 });
 
-export const { addFavorite } = favoriteSlice.actions;
+export const { addElement } = favoriteSlice.actions;
+
+export const selectFavorites = (state) => state.favorite;
+
+export const addFavorite = (element) => (dispatch, getState) => {
+  const currentValue = selectFavorites(getState());
+  if (currentValue.length === 0) {
+    dispatch(addElement(element));
+  } else if (!currentValue.some((x) => element.name === x.name)) {
+    dispatch(addElement(element));
+  }
+};
 
 export default favoriteSlice.reducer;
