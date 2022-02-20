e09e5fb3f4c4c3716a8278fe7905c5259656daf8
Francis <fmoscatelli@hotmail.com>
2022-02-20 12:53:27 -0300

    add text utils

---
+++ src/features/utils.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+export const Capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+export const HyphenToSpace = (str) => str.replaceAll('-', ' ');
