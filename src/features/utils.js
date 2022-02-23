3d8932bbac77dd1c51075bbc4f803cc65c6e7388
Francis <fmoscatelli@hotmail.com>
2022-02-23 13:05:41 -0300

    add NormalizeFistWord

--- src/features/utils.js
+++ src/features/utils.js
@@ -2,3 +2,5 @@
 export const Capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
 export const HyphenToSpace = (str) => str.replaceAll('-', ' ');
+
+export const NormalizeFistWord = (str) => Capitalize(HyphenToSpace(str));
