04f448df131e589311166568c06bc41a4c410409
Francis <fmoscatelli@hotmail.com>
2022-02-23 16:56:06 -0300

    add delete favorite

--- src/features/favorite/favoriteSlice.js
+++ src/features/favorite/favoriteSlice.js
@@ -7,10 +7,18 @@ export const favoriteSlice = createSlice({
     addElement: (state, action) => {
       state.push(action.payload);
     },
+    deleteFavorite: (state, action) => {
+      const index = state.findIndex(
+        (favorite) => favorite.number === action.payload,
+      );
+      if (index > -1) {
+        state.splice(index, 1);
+      }
+    },
   },
 });
 
-export const { addElement } = favoriteSlice.actions;
+export const { addElement, deleteFavorite } = favoriteSlice.actions;
 
 export const selectFavorites = (state) => state.favorite;
 
